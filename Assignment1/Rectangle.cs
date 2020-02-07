465ea5a620f0ef486f35bd56d8e582dce7c319ef
Manjinder Singh <Manjindersingh8670@conestogac.on.ca>
2020-02-06 22:16:59 -0500

    Added project solution folder

--- Assignment1/Rectangle.cs
+++ Assignment1/Rectangle.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace Assignment1
 {
-    class Program
+    class Rectangle
     {
-        static void Main(string[] args)
-        {
-            Rectangle rectangle = new Rectangle();
-        }
     }
 }
